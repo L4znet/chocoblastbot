502e2f1bebd520acf15c966e2cf948ab717ad31c
Charly Escalona <charly.escalona1@gmail.com>
2024-09-05 10:31:42 +0200

    refacto and big eyes

--- commands/cowsays.mjs
+++ commands/cowsays.mjs
@@ -2,6 +2,8 @@ import * as cowsay from "cowsay";
 
 
 export const cowsaybigeyes = (text) => {
+
+    console.log("fsdmlkfdsmlkjsfdqlkjqfdsmlkjfslkdqjmkfj")
     return cowsay.say({
         text: text,
         f: 'eyes'
