2d8de6548725a7dfd33d0304d380dceaee91200a
remybms <bamasremy43@gmail.com>
2024-10-31 16:37:49 +0100

    correct bug chocoblast without ciminal

--- commands/chocoblast.js
+++ commands/chocoblast.js
@@ -1,6 +1,6 @@
-export const chocoblast = (victime, author = "") => {
+export const chocoblast = (victime, author = null) => {
 
-    if(author === ""){
+    if(author === null){
         return `🔥 Voilà quelque chose de spécial pour vous : **${victime}** s'est fait chocoblast 🍫 !`
     } else {
         return `🔥 Voilà quelque chose de spécial pour vous : **${victime}** s'est fait chocoblast 🍫 par **${author}** !`;
