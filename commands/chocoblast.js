57d5e1d950758f814a2668cbbf05e31dc7867875
remybms <bamasremy43@gmail.com>
2024-11-06 14:40:38 +0100

    trying debug chocoblast without author

--- commands/chocoblast.js
+++ commands/chocoblast.js
@@ -1,6 +1,6 @@
 export const chocoblast = (victime, author = null) => {
 
-    if(author === null){
+    if(author !== null){
         return `🔥 Voilà quelque chose de spécial pour vous : **${victime}** s'est fait chocoblast 🍫 !`
     } else {
         return `🔥 Voilà quelque chose de spécial pour vous : **${victime}** s'est fait chocoblast 🍫 par **${author}** !`;
